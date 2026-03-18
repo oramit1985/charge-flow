54b5f29d4e66738dc31e4982cc45b92d65d94bc9
Oramit <or.amit@namogoo.com>
2026-03-18 14:59:44 +0200

    update sqs env variables

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,6 +1,5 @@
 export * from './dto/address.dto';
 export * from './dto/order.dto';
-export * from './events/order-events';
 export * from './errors/app.error';
 export * from './common-types/enums/error-codes';
-export * from '../../filters/http-exception.filter';
+export * from './filters/http-exception.filter';
